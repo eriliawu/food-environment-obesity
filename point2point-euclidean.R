b076785d89e3c5ae13a4640af3c6bd4729c6a998
Erilia Wu <eriliawu@users.noreply.github.com>
2018-07-04 22:31:00 -0400

    Update point2point-euclidean.R

--- point2point-euclidean.R
+++ point2point-euclidean.R
@@ -11,11 +11,12 @@ suppressWarnings(library(rgeos))
 suppressWarnings(library(rgdal))
 suppressWarnings(library(spdep))
 suppressWarnings(library(measurements)) #to convert imperial/metric units
-suppressWarnings(library(readstata13))
+suppressWarnings(library(readstata13)) #I wish economists didnt like stata this much
 
 ### calculate distance to nearest food outlet ----
 school.all <- NULL
-# separate into 4 outlet types
+# loop AY 09-13 of schools and their x-y coordinates
+# for each year of food sources, separate into 4 outlet types
 for (i in c("09", "10", "11", "12", "13")) {
       # read school address data
       school <- read.csv("xy-coord_2009-2013.csv", stringsAsFactors=FALSE)
@@ -31,7 +32,7 @@ for (i in c("09", "10", "11", "12", "13")) {
       # read food outlet data
       food <- read.csv(paste("H:/Personal/food environment paper 1/restaurants/restaurants", i, ".csv", sep = ""), stringsAsFactors=FALSE)
       food <- na.omit(food)
-      for (j in c("FFOR", "BOD", "WS", "C6P")) { # separate food outlets into 4 diff types and calculate dist to nearest of each type
+      for (j in c("FFOR", "BOD", "WS", "C6P")) { #separate food outlets into 4 diff types and calculate dist to nearest of each type
             food1 <- food[food$cat2==j, ]
             food1 <- ppp(food1$x, food1$y, 
                         window=owin(xrange=c(0, max(food1$x)), 
